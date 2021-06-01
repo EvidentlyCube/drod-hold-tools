17e30c92cb4f3520f0d727cf06f201f88868a8f8
Maurycy Zarzycki <maurycy@evidentlycube.com>
2021-06-01 17:36:05 +0200

    Pending changes widget now has buttons for exporting and closing hold

--- src/tabs/HoldPendingChanges.tsx
+++ src/tabs/HoldPendingChanges.tsx
@@ -1,12 +1,34 @@
-import {createStyles, List, ListItem, ListItemText, Paper, Theme, Typography, WithStyles, withStyles} from "@material-ui/core";
+import {
+	Box,
+	Button,
+	createStyles,
+	Dialog, DialogActions, DialogContent, DialogContentText,
+	DialogTitle,
+	Divider,
+	List,
+	ListItem,
+	ListItemText,
+	Paper,
+	Theme,
+	Typography,
+	WithStyles,
+	withStyles,
+} from "@material-ui/core";
 import React from "react";
-import {Hold} from "../data/Hold";
+import {createNullHold, Hold} from "../data/Hold";
 import {Change} from "../data/Change";
+import {Store} from "../data/Store";
 
 const styles = (theme: Theme) => createStyles({
 	content: {
-		padding: theme.spacing(8, 0, 6),
+		padding: theme.spacing(4, 6),
 	},
+	actions: {
+		textAlign: 'center',
+		'& > *': {
+			margin: theme.spacing(1)
+		}
+	}
 });
 
 interface HoldPendingChangesProps extends WithStyles<typeof styles> {
@@ -14,13 +36,51 @@ interface HoldPendingChangesProps extends WithStyles<typeof styles> {
 }
 
 interface HoldPendingChangesState {
+	showExport: boolean;
+	showClose: boolean;
 }
 
 class HoldPendingChanges extends React.Component<HoldPendingChangesProps, HoldPendingChangesState> {
 	constructor(props: Readonly<HoldPendingChangesProps> | HoldPendingChangesProps) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			showExport: false,
+			showClose: false
+		};
+	}
+
+	private onExport = () => {
+		const {showExport} = this.state;
+
+		if (showExport) {
+			alert("HOLD ENCODER ENGAGE");
+			this.setState({showExport: false});
+
+		} else {
+			this.setState({showExport: true});
+		}
+	}
+
+	private onCloseHold = () =>
+	{
+		const {hold} = this.props;
+		const {showClose} = this.state;
+
+		if (hold.changes.size === 0 || showClose) {
+			Store.loadedHold.value = createNullHold();
+			this.setState({showClose: false});
+
+		} else {
+			this.setState({showClose: true});
+		}
+	}
+
+	private onCancelDialog = () => {
+		this.setState({
+			showExport: false,
+			showClose: false
+		});
 	}
 
 	renderChange(change: Change, id: number) {
@@ -51,6 +111,7 @@ class HoldPendingChanges extends React.Component<HoldPendingChangesProps, HoldPe
 
 	render() {
 		const {hold, classes} = this.props;
+		const {showClose, showExport} = this.state;
 
 		return <Paper className={classes.content}>
 			<Typography variant="h5" noWrap align="center">
@@ -59,6 +120,53 @@ class HoldPendingChanges extends React.Component<HoldPendingChangesProps, HoldPe
 			<List>
 				{Array.from(hold.changes).map((change, index) => this.renderChange(change, index))}
 			</List>
+			<Divider/>
+			<Box className={classes.actions}>
+				<Button variant="contained" color="primary" onClick={this.onExport}>Export Hold with Changes</Button>
+				<Button variant="contained" color="secondary" onClick={this.onCloseHold}>Close Hold</Button>
+			</Box>
+			<Dialog
+				disableBackdropClick
+				disableEscapeKeyDown
+				maxWidth="xs"
+				open={showExport}
+			>
+				<DialogTitle>Are you sure you want to export the hold?</DialogTitle>
+				<DialogContent dividers>
+					<DialogContentText>
+						This will commit all of the pending changes. This operatin cannot be undone!
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button autoFocus onClick={this.onCancelDialog} color="primary">
+						Cancel
+					</Button>
+					<Button onClick={this.onExport} color="primary">
+						Ok
+					</Button>
+				</DialogActions>
+			</Dialog>
+			<Dialog
+				disableBackdropClick
+				disableEscapeKeyDown
+				maxWidth="xs"
+				open={showClose}
+			>
+				<DialogTitle>Are you sure you want to close the hold?</DialogTitle>
+				<DialogContent dividers>
+					<DialogContentText>
+						You have unsaved changes and they will be lost.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button autoFocus onClick={this.onCancelDialog} color="primary">
+						Cancel
+					</Button>
+					<Button onClick={this.onCloseHold} color="primary">
+						Ok
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</Paper>;
 	}
 }
